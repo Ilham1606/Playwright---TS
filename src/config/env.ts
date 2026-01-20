c2c1bca4fc5ee0fef7e63f9b6e5b6246eada15ed
Ilham Apriansyah <16ilhamapriansyah@gmail.com>
2026-01-20 16:07:34 +0700

    update env.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -7,7 +7,13 @@ function requireEnv(name: string): string {
 }
 
 export const ENV = {
-  BASE_URL_CONDUIT: requireEnv("BASE_URL_CONDUIT"),
-  BASE_URL_DUMMYJSON: requireEnv("BASE_URL_DUMMYJSON"),
+  BASE_URL_CONDUIT: process.env.CI
+    ? "https://playground.bondaracademy.com"
+    : requireEnv("BASE_URL_CONDUIT"),
+
+  BASE_URL_DUMMYJSON: process.env.CI
+    ? "https://dummyjson.com"
+    : requireEnv("BASE_URL_DUMMYJSON"),
+
   IS_CI: !!process.env.CI,
 };
